96e6a6a8016ed8c400b5b641c0a69d1122a85ce5
Yasi <Yasersajed23@gmail.com>
2026-01-22 10:16:58 +0100

    actualizo y mejoro el index.ts

--- index.ts
+++ index.ts
@@ -1,13 +1,34 @@
 import { initDB } from "./db/init"
 import { getAllMovies, getMovieByTitle } from "./models/movies"
 import express from "express"
+import type { Request, Response, NextFunction} from "express"
 const app = express();
 const PORT = 3000
 const db = await initDB()
 
+
+const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    //codigo
+     console.log(req.method, req.url, new Date().toISOString())
+    if(req.method === "GET"){
+        console.log(req.query)
+    }
+     next()
+}
+
+app.use(logMiddleware)
+
 app.get("/movies", (req, res)=> {
-    const peliculas = getAllMovies(db)
+    const title = req.query.title
+    if(title){
+    console.log("Estoy dentro del endpoint", title)
+    const peliculas = getMovieByTitle(db, title)
     res.json(peliculas)
+    }
+    else {
+        const peliculas = getAllMovies(db)
+        res.json(peliculas)
+    }
 })
 
 app.listen(PORT, ()=> {
